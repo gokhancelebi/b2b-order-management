01aa1bb3c82aee06f0413154b1746e2e6b3c2271
Gökhan Çelebi <dinatopya03@gmail.com>
2025-08-04 13:44:38 +0300

    User controller - service implementation

--- B2BOrderManagement/Controllers/UserController.cs
+++ B2BOrderManagement/Controllers/UserController.cs
@@ -1,6 +1,43 @@
-﻿namespace B2BOrderManagement.Controllers
+﻿using B2BOrderManagement.Models;
+using B2BOrderManagement.Services;
+using Microsoft.AspNetCore.Mvc; 
+
+
+namespace B2BOrderManagement.Controllers
 {
-    public class UserController
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
     {
+        private UserService _service { get; set; }
+        public UserController(UserService service) {
+            _service = service;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var list = await _service.GetAllUsersAsync();
+            return Ok(new { message = "merhaba", date = list });
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] User user)
+        {
+            var created = await _service.AddUserAsync(user);
+            return Ok(created);
+        }
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, [FromBody] User user)
+        {
+            var updated = await _service.UpdateUserAsync(user);
+            if (updated == null) return NotFound();
+            return Ok(updated);
+        }
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var deleted = await _service.DeleteUserAsync(id);
+            if(!deleted) return NotFound();
+            return Ok(deleted);
+        }
     }
 }
