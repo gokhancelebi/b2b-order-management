ab96ad22fa22db27a8bb789f64c423a14716877c
Gökhan Çelebi <dinatopya03@gmail.com>
2025-08-07 18:40:49 +0300

    Refactor AppDbContext to replace Attribute and AttributeValue with ProductAttribute and ProductAttributeValue; add DbSet properties for Variants and VariantAttributeValues.

--- B2BOrderManagement/Data/AppDbContext.cs
+++ B2BOrderManagement/Data/AppDbContext.cs
@@ -2,7 +2,7 @@
 using B2BOrderManagement.Models.User;
 using B2BOrderManagement.Models.Product;
 using B2BOrderManagement.Models.Category;
-using B2BOrderManagement.Models.Attribute.Attribute;
+using B2BOrderManagement.Models.Attribute;
 using B2BOrderManagement.Models.TaxClass;
 using B2BOrderManagement.Models;
 using B2BOrderManagement.Models.Order;
@@ -18,10 +18,12 @@ namespace B2BOrderManagement.Data
         public DbSet<User> Users { get; set; }
         public DbSet<Product> Products { get; set; }
         public DbSet<Category> Categories { get; set; }
-        public DbSet<Attribute> Attributes { get; set; }
-        public DbSet<AttributeValue> AttributeValues { get; set; }
+        public DbSet<ProductAttribute> ProductAttributes { get; set; }
+        public DbSet<ProductAttributeValue> ProductAttributeValues { get; set; }
         public DbSet<TaxClass> TaxClasses { get; set; }
         public DbSet<WebsiteOption> WebsiteOptions { get; set; }
         public DbSet<Order> Orders { get; set; }
+        public DbSet<Variant> Variants { get; set; }
+        public DbSet<VariantAttributeValue> VariantAttributeValues { get; set; }
     }
 }
